dd2cfee07e1107a0fdf48a99c260a6b1ade6e293
juli-nvl <julianeneveling@gmail.com>
2025-02-17 18:17:15 +0100

    added real time date

--- weatherapp.js
+++ weatherapp.js
@@ -1,12 +1,33 @@
 function refreshData(response) {
   let temperature = document.querySelector("#now-temperature");
   temperature.innerHTML = Math.round(response.data.temperature.current);
+  let date = new Date(response.data.time * 1000);
+  let time = document.querySelector("#time");
+  time.innerHTML = formatDate(date);
   let condition = document.querySelector("#now-condition");
   condition.innerHTML = response.data.condition.description;
   let humidity = document.querySelector("#now-humidity");
   humidity.innerHTML = response.data.temperature.humidity;
   let windspeed = document.querySelector("#now-windspeed");
   windspeed.innerHTML = response.data.wind.speed;
+}
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
