00ae2c3636dc3e9440dc6b457a5868a9d4409fab
juli-nvl <julianeneveling@gmail.com>
2025-02-17 15:13:32 +0100

    added js to show current temperature

--- weatherapp.js
+++ weatherapp.js
@@ -1,8 +1,20 @@
+function refreshData(response) {
+  let temperature = document.querySelector("#data-now-temperature");
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "7043o377bb0t07ffedfea40179990f35";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshData);
+}
+
 function submitCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let city = document.querySelector("#city");
   city.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchElement = document.querySelector("#search-element");
